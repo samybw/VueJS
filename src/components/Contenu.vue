8d91d3a4904f549ecae74ee396e506390a46ceb4
Samybw <samybn@yahoo.com>
2020-07-14 11:27:07 +0200

    Creat form with vue

--- src/components/Contenu.vue
+++ src/components/Contenu.vue
@@ -1,8 +1,64 @@
 <template>
   <div class="container">
-   <h1>Appel d'une API</h1>
+   <h1>Formulaire</h1>
 
-    <img v-bind:src="urlImg">
+   <form >
+   
+   <label for="prenom"> Prenom </label>
+  <input v-on:input="toggleResult" v-model="formData.prenom" type="prenom" name="prenom" id="prenom">
+  <br>
+  <br>
+  <label for="txt"> Textarea</label>
+  <textarea v-model="formData.txt" id="txt" ></textarea>
+   
+
+
+
+            <h3>Checkbox</h3>
+               <div>
+     <label for="fraise"> Fraise</label>
+     <input v-model="formData.checkFruits" value="fraise" type="checkbox"  id="fraise">
+
+   </div>
+
+    <div>
+     <label for="pomme"> Pomme </label>
+     <input v-model="formData.checkFruits" value="pomme" type="checkbox"  id="pomme">
+
+   </div>
+   
+    <div>
+     <label for="cerises"> Cerises</label>
+     <input v-model="formData.checkFruits" value="cerises" type="checkbox"  id="cerises">
+
+
+        
+        <h3>Select Box </h3>
+        <select v-model="formData.select">
+          <option v-for="(pays, index) in formData.listePays " :key="index" > {{ pays }}</option>
+        </select>
+
+         
+   </div>
+    <br>
+    <button v-on:click.prevent="envoiForm" type="submit"> Envoyer les données</button>
+   <br>
+   </form>
+   
+   <br>
+
+   <div v-if="infoSubmit">
+      <h2>Resultat</h2>
+
+   <p>Prenom : {{ formData.prenom }}</p>
+   <p>Text : {{ formData.txt }}</p>
+          <ul>
+         <li v-for="(fruits, index) in formData.checkFruits" :key="index"> {{ fruits }} </li>
+       </ul>
+   <p>Pays : {{ formData.select}}</p>
+
+   </div>
+
 
 
   </div>
@@ -10,28 +66,34 @@
 </template>
 <script> 
 
-import axios from 'axios'
 
       export default {
           name: 'continue',
           data: function () {
               return {
-                 urlImg : null,
+                formData: {
+                 prenom : '',
+                 txt: '',
+                 checkFruits : [],
+                 select: '',
+                 listePays : ['CongoRDC' ,'Chine','Sénégal','USA']
+                },
+                infoSubmit: false
 
               }
                 
            },
            methods: {
+             envoiForm: function (){
+                this.infoSubmit = true;
+             },
+
+             toggleResult: function () {
+               this.infoSubmit = false
+             }
            
            },
-           mounted() {
-             axios
-             .get('https://api.thecatapi.com/v1/images/search')
-             .then(response => {
-               console.log(response);
-               this.urlImg = response.data[0].url
-             }) 
-           },
+    
         }
 
 
